324a49855f1f8750aae420b327edc59b02882d64
Haiul Sk <haiulsk037@gmail.com>
2025-10-14 23:59:52 +0530

    *

--- Online-Food-Ordering/src/main/java/com/haiul/Online/Food/Ordering/repository/FoodRepository.java
+++ Online-Food-Ordering/src/main/java/com/haiul/Online/Food/Ordering/repository/FoodRepository.java
@@ -1,6 +1,16 @@
 package com.haiul.Online.Food.Ordering.repository;
 
-public interface FoodRepository {
+import com.haiul.Online.Food.Ordering.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
+public interface FoodRepository extends JpaRepository<Food,Long> {
+
+    List<Food> findbyRestaurantId(Long restaurantId);
+
+    @Query("SELECT FROM Food f WHERE f.neme LIKE N:keyword's OR f.foodCategory.name LIKE %:keywords")
+    List<Food> searchFood(@Param(("keyword"))String keyword);
 }
