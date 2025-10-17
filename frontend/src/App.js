99434e10c2898f86ed2e942cd09088daf50224dd
Haiul Sk <haiulsk037@gmail.com>
2025-10-17 20:19:20 +0530

    *

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='text-red-500'>
-      Hello
+    <div className='text-amber-500'>
+      <h1 className='text-red-700'>Hello Haiul!</h1>
     </div>
   );
 }
