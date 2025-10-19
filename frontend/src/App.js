a57fb9bf3ebbf0226c629e4ab276bdc17dd45059
Haiul Sk <haiulsk037@gmail.com>
2025-10-19 23:56:21 +0530

    *

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import {darkTheme} from './theme/DarkTheme'
 import Home from './components/Home/Home';
 import RestaurantDetails from './components/RestaurantCard/RestaurantDetails';
+import CartItem from './components/cart/CartItem';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <CssBaseline/>
       <Navbar/>
       {/* <Home/> */}
-    <RestaurantDetails/>
+  {/* <RestaurantDetails/> */}
+  <CartItem/>
     </ThemeProvider>
   );
 }
