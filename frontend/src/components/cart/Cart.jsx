23a6f28797aeb115117e5be251f058cfd81f2055
Haiul Sk <haiulsk037@gmail.com>
2025-10-20 00:21:23 +0530

    *

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CartItem from './CartItem'
+import { Divider } from '@mui/material';
 
 const items = [1,1];
 
@@ -15,6 +16,36 @@ const Cart = () => {
         </section>
 
         <Divider/>
+
+        <div className='billDetails px-5 text-sm'>
+          <p className='font-extralight py-5'>Bill Details</p>
+          <div className='flex justify-between text-gray-400'>
+            <p>Item Total</p>
+            <p>$599</p>
+          </div>
+
+          <div className='flex justify-between text-gray-400'>
+            <p>Delivery Fee</p>
+            <p>$59</p>
+          </div>
+
+          <div className='flex justify-between text-gray-400'>
+            <p>Platform Fee</p>
+            <p>$599</p>
+          </div>
+
+           <div className='flex justify-between text-gray-400'>
+            <p>GST and Restaurant Charges</p>
+            <p>$599</p>
+          </div> 
+
+          <Divider/>
+        </div>
+
+        <div className='flex justify-between text-gray-400'>
+          <p>Total Pay</p>
+          <p>$3000</p>
+        </div>
       </main>
     </div>
   )
